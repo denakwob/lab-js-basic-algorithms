12a396e249033ff4c61fa7d457f3eb412b38beac
nanaobengkweku <nanaobengkweku@gmail.com>
2024-07-20 01:50:40 +0000

    done with all task

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // Iteration 1: Names and Input
 let hacker1 = "Detard";
 let hacker2 = "Le Louch";
+
 console.log("The driver's name is " + hacker1)
 console.log("The navigator's name is " + hacker2)
 
@@ -32,23 +33,46 @@ if (hacker1 < hacker2){
 } else {
     console.log("What? You both have the same name?")
 }
+console.log(spacedCapsName);
+console.log(reversedName);
 
 // Bonus 1
 let loremString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque viverra nulla ac molestie congue. Vivamus mollis sem id convallis vulputate. Fusce ultrices in enim quis varius. Nulla ultricies sollicitudin enim at et egestas. Quisque nulla nisl, pellentesque in felis in, placerat congue nulla. Ut molestie varius risus. Quisque pretium rhoncus tempus. Donec iaculis sodales tempus.Nulla facilisi. Morbi placerat, neque non aliquet placerat, ligula velit malesuada lorem, rutrum hendrerit nisl odio sit amet dui. Proin et eros aliquet, imperdiet lacus eget, imperdiet ex. Sed vestibulum risus sit amet enim aliquam feugiat. Vestibulum eu purus a mi luctus hendrerit. Ut vel sodales quam. Nunc enim massa, pellentesque sed tellus luctus, dictum luctus tellus. Etiam ut arcu eu risus tempor pretium. Nulla vitae condimentum augue, a dapibus augue. Nullam egestas libero sit amet ante sagittis laoreet. Maecenas vel tempor ligula, nec laoreet neque. Pellentesque vehicula consectetur commodo. Nam sed blandit purus, eu efficitur ante. Fusce ullamcorper dui sed facilisis rhoncus.Donec tortor ante, condimentum eget orci tristique, accumsan interdum libero. Nunc at dolor vitae urna congue imperdiet. Praesent id facilisis leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas nibh elit, blandit vel efficitur in, efficitur id felis. Mauris lacinia dui quis odio luctus porttitor. Aenean consectetur rutrum tortor, non pretium diam vestibulum quis. Nunc blandit nisl sed lectus viverra imperdiet. Proin varius aliquet mattis. Maecenas lacinia vel nibh eget sollicitudin. Maecenas sodales rutrum purus quis malesuada."
 
 let words = loremString.split(" ");
-let wordLen = words.length;
+let wordsLength = words.length;
 let count = 0;
 
-for (let i = 0; i < wordLen; i++){
+for (let i = 0; i < wordsLength; i++){
     if (words[i] === 'et'){
         count += 1;
     }
 }
-
+console.log(wordsLength);
 console.log(count);
 
-console.log(spacedCapsName);
-console.log(reversedName);
+// Bonus 2:
+let phraseToCheck = 'stack cats';
+let reversedPhrase = phraseToCheck
+    .split('')
+    .filter(char => /[a-zA-Z0-9]/.test(char)).
+    reverse().
+    join('').
+    toLowerCase()
+;
+
+phraseToCheck = phraseToCheck
+    .split('')
+    .filter(char => /[a-zA-Z0-9]/.test(char))
+    .join("")
+    .toLowerCase()
+;
+
+if (phraseToCheck === reversedPhrase){
+    console.log("It's a palindrome");
+} else {
+    console.log("oops, this is not a palindrome");
+}
+
 
 
